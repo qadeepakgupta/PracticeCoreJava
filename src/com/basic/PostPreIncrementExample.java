2afd34784cd1a94ed506a7732667a1e3b942c586
qadeepakgupta <qadeepakgupta@gmail.com>
2017-09-05 15:14:02 +0530

    Test

---
+++ src/com/basic/PostPreIncrementExample.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class PostPreIncrementExample {
+
+	public static void main(String[] args) {
+		int a = 0;
+		System.out.println(a++); // POST INCREMENT: prints first, then increments
+		System.out.println(a);
+		System.out.println(++a); // PRE INCREMENT: increments first, then prints
+	}
+
+}
--- src/com/basic/TestDemo.java
+++
@@ -1,9 +0,0 @@
-package com.basic;
-
-public class TestDemo {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-
-}
