02cfe30269c300a6078b0aa43d4a3d93ff084122
qadeepakgupta <qadeepakgupta@gmail.com>
2017-09-05 15:17:16 +0530

    w

---
+++ src/com/basic/DoWhileExample1.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class DoWhileExample1 {
+	public static void main(String[] args) {
+
+		int a = 10;
+		/* do loop execution test */
+		do {
+			System.out.println("value of a: " + a);
+			a = a + 1;
+		} while (a < 20);
+	}
+}
